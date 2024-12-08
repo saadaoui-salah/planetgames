999dcf20a2ccbf7b06c7a7119ac2dee394197608
saadaoui-salah <salahsaadaoui8@gmail.com>
2024-12-08 03:30:15 -0500

    add CSRF_TRUSTED_ORIGINS

--- backend/core/settings.py
+++ backend/core/settings.py
@@ -171,7 +171,13 @@ USE_TZ = True
 CORS_ALLOW_CREDENTIALS = True
 CORS_ALLOWED_ORIGINS = [
     'http://localhost:3000',
-    'http://planetgamesshop.vercel.app'
+    'http://planetgamesshop.vercel.app',
+    'https://dashboard-web-copy-production.up.railway.app'
+]
+CSRF_TRUSTED_ORIGINS = [
+    'http://localhost:3000',
+    'http://planetgamesshop.vercel.app',
+    'https://dashboard-web-copy-production.up.railway.app',
 ]
 
 REST_FRAMEWORK = {
